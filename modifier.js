86682525bd54739cf8a5bda3decd7444e155baf1
martin-orona <32714682+martin-orona@users.noreply.github.com>
2019-12-11 11:29:49 -0800

    Update modifier.js

--- modifier.js
+++ modifier.js
@@ -1,6 +1,9 @@
 function modifyButton(selector) {
   var button = document.querySelector(selector);
+  var initial = button.innerHTML;
   button.innerHTML += " modified";
+  var final = button.innerHTML;
+  console.log("modified element content from [" + initial + "] to [" + final + "]");
 }
 
 modifyButton("#gatewayButtonDiv > a");
